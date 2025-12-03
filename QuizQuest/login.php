f25237e4f4c9529f65a9d2a39058bd1115cc2a4a
graven <shinominecro@gmail.com>
2025-12-03 18:48:24 +0800

    changing again

--- QuizQuest/login.php
+++ QuizQuest/login.php
@@ -49,13 +49,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["cancel"])) {
     <title>QuizQuest Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="QuizQuest/assets/css/login.css">
+    <link rel="stylesheet" href="assets/css/login.css">
 </head>
 <body>
 
 <header class="header">
     <div class="logo-container">
-        <img src="QuizQuest/assets/images/logo.png" alt="QuizQuest Logo">
+        <img src="assets/images/logo.png" alt="QuizQuest Logo">
     </div>
 </header>
 
@@ -104,7 +104,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["cancel"])) {
         <div class="right-side">
 
             <div class="title">
-                <img src="\assets/images/quizquest-title.png">
+                <img src="assets/images/quizquest-title.png">
             </div>
             
             <p class="subheading">Where every quiz is an adventure!</p>
--- QuizQuest/register_student.php
+++ QuizQuest/register_student.php
@@ -69,14 +69,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["register"])) {
     <title>QuizQuest Register</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="QuizQuest/assets/css/register_student.css">
+    <link rel="stylesheet" href="assets/css/register_student.css">
 </head>
 
 <body>
 
 <header class="header">
     <div class="logo-container">
-        <img src="QuizQuest/assets/images/logo.png" alt="QuizQuest Logo">
+        <img src="assets/images/logo.png" alt="QuizQuest Logo">
     </div>
 </header>
 
@@ -118,7 +118,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["register"])) {
         <div class="right-side">
 
             <div class="title">
-                <img src="QuizQuest/assets/images/quizquest-title.png">
+                <img src="assets/images/quizquest-title.png">
             </div>
             <p class="subheading">Where every quiz is an adventure!</p>
 
