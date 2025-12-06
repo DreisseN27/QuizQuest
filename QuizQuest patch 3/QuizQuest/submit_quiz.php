1928c2f49b25aa52afc32fb5deb09acd29d4c532
graven <shinominecro@gmail.com>
2025-12-06 18:08:17 +0800

    editing

--- QuizQuest patch 3/QuizQuest/submit_quiz.php
+++ QuizQuest patch 3/QuizQuest/submit_quiz.php
@@ -152,8 +152,8 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <title>Quiz Result</title>
-    <link rel="stylesheet" href="assets/css/submit_quiz.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/submit_quiz.css">
 </head>
 
 <body>
